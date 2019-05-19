06299006d0dde18d9d8d9b0f648cb076d999d268
Rob Hess <robwhess@gmail.com>
2019-05-19 13:45:35 -0700

    Add image DB functions and GET /images/{id} endpoint.

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 
 const { connectToDB } = require('./lib/mongo');
+const { getImageInfoById } = require('./models/image');
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.get('/', (req, res, next) => {
   res.status(200).sendFile(__dirname + '/index.html');
+});
+
+app.get('/images/:id', async (req, res, next) => {
+  try {
+    const image = await getImageInfoById(req.params.id);
+    if (image) {
+      res.status(200).send(image);
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 app.use('*', (req, res, next) => {
